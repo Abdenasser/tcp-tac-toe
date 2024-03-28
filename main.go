07216b893ab17f8fa7ab7204820ab433801690bc
Abdenasser <nasser.elidrissi065@gmail.com>
2024-03-28 02:16:00 +0000

    creates a slice of connections and players to keep track of connected clients

--- main.go
+++ main.go
@@ -5,6 +5,33 @@ import (
 	"net"
 )
 
+var playerSymbols = []string{"x", "o"}
+var connections = []net.Conn{}
+
+type Player struct {
+	Connection net.Conn
+	Symbol     string
+	Score      int
+}
+
+// will only handle 2 players for now
+var players = []Player{}
+
+func popSymbol(s *[]string) string {
+	pop := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return pop
+}
+
+func createPlayer(c net.Conn) Player {
+	player := Player{
+		Connection: c,
+		Symbol:     popSymbol(&playerSymbols),
+		Score:      0,
+	}
+	return player
+}
+
 func main() {
 	listener, _ := net.Listen("tcp", "localhost:8080")
 	fmt.Printf("Listening on %s.\n", "localhost:8080")
@@ -15,5 +42,10 @@ func main() {
 			fmt.Printf("Some connection error: %s\n", err)
 		}
 		fmt.Printf("client connected from %v\n", conn.RemoteAddr().String())
+		connections = append(connections, conn)
+		if len(players) < 2 {
+			players = append(players, createPlayer(conn))
+		}
+		fmt.Printf("clients %v, players %v, symbols %v\n", len(connections), len(players), len(playerSymbols))
 	}
 }
