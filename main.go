bdcfa7afe2afb955500dbab9f69cb33c0ee7e89c
Abdenasser <nasser.elidrissi065@gmail.com>
2024-03-28 02:36:03 +0000

    handing client disconnection

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 var playerSymbols = []string{"x", "o"}
@@ -32,9 +33,43 @@ func createPlayer(c net.Conn) Player {
 	return player
 }
 
+func handleConnection(conn net.Conn) {
+	fmt.Printf("Client connected from %v \n", conn.RemoteAddr().String())
+	time.Sleep(5 * time.Second)
+	closeConnection(conn)
+}
+
+func closeConnection(conn net.Conn) {
+	removeConn(conn)
+	removePlayer(conn)
+	fmt.Printf("Client at %v disconnected.\n", conn.RemoteAddr().String())
+	conn.Close()
+}
+
+func removeConn(c net.Conn) {
+	for i, conn := range connections {
+		if conn == c {
+			connections = append(connections[:i], connections[i+1:]...)
+			break
+		}
+	}
+}
+
+func removePlayer(c net.Conn) {
+	for i, p := range players {
+		if p.Connection == c {
+			playerSymbols = append(playerSymbols, p.Symbol)
+			players = append(players[:i], players[i+1:]...)
+			break
+		}
+	}
+}
+
 func main() {
 	listener, _ := net.Listen("tcp", "localhost:8080")
 	fmt.Printf("Listening on %s.\n", "localhost:8080")
+
+	defer listener.Close()
 
 	for {
 		conn, err := listener.Accept() // connect using telnet cmd: telnet localhost 8080
@@ -46,6 +81,8 @@ func main() {
 		if len(players) < 2 {
 			players = append(players, createPlayer(conn))
 		}
+		// accept inputs from connected clients in a separate goroutine
+		go handleConnection(conn)
 		fmt.Printf("clients %v, players %v, symbols %v\n", len(connections), len(players), len(playerSymbols))
 	}
 }
