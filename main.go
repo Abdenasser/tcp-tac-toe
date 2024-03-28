7ca6329f4480d9554102b27616a98c32f8ede1fe
Abdenasser <nasser.elidrissi065@gmail.com>
2024-03-28 04:21:31 +0000

    getting there...hmmm

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
-	"time"
+	"strconv"
 )
-
-var playerSymbols = []string{"x", "o"}
-var connections = []net.Conn{}
 
 type Player struct {
 	Connection net.Conn
@@ -17,58 +15,76 @@ type Player struct {
 
 // will only handle 2 players for now
 var players = []Player{}
-
-func popSymbol(s *[]string) string {
-	pop := (*s)[len(*s)-1]
-	*s = (*s)[:len(*s)-1]
-	return pop
+var lastPlayed = map[string]int{
+	"playerIndex": 0,
 }
 
-func createPlayer(c net.Conn) Player {
-	player := Player{
-		Connection: c,
-		Symbol:     popSymbol(&playerSymbols),
-		Score:      0,
-	}
-	return player
-}
-
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, b map[int]string) {
 	fmt.Printf("Client connected from %v \n", conn.RemoteAddr().String())
-	time.Sleep(5 * time.Second)
-	closeConnection(conn)
-}
-
-func closeConnection(conn net.Conn) {
-	removeConn(conn)
+	scanner := bufio.NewScanner(conn)
+	for {
+		// output := printBoard(b)
+		// conn.Write([]byte(output + "\n"))
+		conn.Write([]byte("pick a position please \n"))
+		ok := scanner.Scan()
+		if !ok {
+			break
+		}
+		play(scanner.Text(), b, players, conn)
+	}
 	removePlayer(conn)
 	fmt.Printf("Client at %v disconnected.\n", conn.RemoteAddr().String())
 	conn.Close()
 }
 
-func removeConn(c net.Conn) {
-	for i, conn := range connections {
-		if conn == c {
-			connections = append(connections[:i], connections[i+1:]...)
-			break
+func play(pos string, b map[int]string, ps []Player, conn net.Conn) {
+	fmt.Println("> " + pos)
+	for _, p := range ps {
+		if lastPlayed["playerIndex"] == 0 {
+			lastPlayed["playerIndex"] = 1
+		} else {
+			lastPlayed["playerIndex"] = 0
 		}
+		player := ps[lastPlayed["playerIndex"]]
+		if p != player {
+			position, _ := strconv.Atoi(pos)
+			b[position] = player.Symbol
+		}
+
+		output := printBoard(b)
+		p.Connection.Write([]byte(output + "\n"))
 	}
 }
 
 func removePlayer(c net.Conn) {
 	for i, p := range players {
 		if p.Connection == c {
-			playerSymbols = append(playerSymbols, p.Symbol)
 			players = append(players[:i], players[i+1:]...)
 			break
 		}
 	}
 }
 
+func initBoard() map[int]string {
+	board := make(map[int]string)
+	for i := 0; i < 9; i++ {
+		board[i] = fmt.Sprintf("%d", i)
+	}
+	return board
+}
+
+func printBoard(b map[int]string) string {
+	output := ""
+	for i := 0; i < 9; i += 3 {
+		output += fmt.Sprintf("%v | %v | %v\n", b[i], b[i+1], b[i+2])
+	}
+	return output
+}
+
 func main() {
 	listener, _ := net.Listen("tcp", "localhost:8080")
 	fmt.Printf("Listening on %s.\n", "localhost:8080")
-
+	b := initBoard()
 	defer listener.Close()
 
 	for {
@@ -76,13 +92,17 @@ func main() {
 		if err != nil {
 			fmt.Printf("Some connection error: %s\n", err)
 		}
+
 		fmt.Printf("client connected from %v\n", conn.RemoteAddr().String())
-		connections = append(connections, conn)
+
 		if len(players) < 2 {
-			players = append(players, createPlayer(conn))
+			player := Player{
+				Connection: conn,
+				Symbol:     []string{"x", "o"}[len(players)],
+				Score:      0,
+			}
+			players = append(players, player)
+			go handleConnection(conn, b)
 		}
-		// accept inputs from connected clients in a separate goroutine
-		go handleConnection(conn)
-		fmt.Printf("clients %v, players %v, symbols %v\n", len(connections), len(players), len(playerSymbols))
 	}
 }
