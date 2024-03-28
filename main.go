184583d392cba18bed6cae6f92a9ce4f2ca94b0b
Abdenasser <nasser.elidrissi065@gmail.com>
2024-03-28 01:30:25 +0000

    an infinite loop for handling multiple client connections

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ func main() {
 	listener, _ := net.Listen("tcp", "localhost:8080")
 	fmt.Printf("Listening on %s.\n", "localhost:8080")
 
-	conn, err := listener.Accept() // connect using telnet cmd: telnet localhost 8080
-	if err != nil {
-		fmt.Printf("Some connection error: %s\n", err)
+	for {
+		conn, err := listener.Accept() // connect using telnet cmd: telnet localhost 8080
+		if err != nil {
+			fmt.Printf("Some connection error: %s\n", err)
+		}
+		fmt.Printf("client connected from %v\n", conn.RemoteAddr().String())
 	}
-	fmt.Printf("client connected from %v\n", conn.RemoteAddr().String())
 }
