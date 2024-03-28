815ac2ab9b8a053128594992b5a8d602f20a1d85
Abdenasser <nasser.elidrissi065@gmail.com>
2024-03-28 01:27:30 +0000

    minimal tcp server that accepts one connection

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, _ := net.Listen("tcp", "localhost:8080")
+	fmt.Printf("Listening on %s.\n", "localhost:8080")
+
+	conn, err := listener.Accept() // connect using telnet cmd: telnet localhost 8080
+	if err != nil {
+		fmt.Printf("Some connection error: %s\n", err)
+	}
+	fmt.Printf("client connected from %v\n", conn.RemoteAddr().String())
+}
