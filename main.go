1174e1a05f6fe7b73a0bb6432d5e2f1c92ef46a4
Abdenasser <nasser.elidrissi065@gmail.com>
2024-03-29 22:17:16 +0000

    1.0.0

--- main.go
+++ main.go
@@ -14,11 +14,107 @@ type Player struct {
 	Score      int
 }
 
-// will only handle 2 players for now
-var players = []Player{}
-var currentPlayer Player
+type Game struct {
+	Board         map[int]string
+	Players       []Player
+	CurrentPlayer Player
+	Watchers      []net.Conn
+}
 
-const NoWinner = "none"
+func (g *Game) printBoard() string {
+	output := ""
+	for i := 0; i < 9; i += 3 {
+		output += fmt.Sprintf("%v | %v | %v\n", g.Board[i], g.Board[i+1], g.Board[i+2])
+	}
+	return output
+}
+
+func (g *Game) isFullBoard() bool {
+	for _, v := range g.Board {
+		if v != colorize("x", "orange") && v != colorize("o", "cyan") {
+			return false
+		}
+	}
+	return true
+}
+
+func (g *Game) shouldResetBoard() bool {
+	if g.getWinner() != "none" || (g.getWinner() == "none" && g.isFullBoard()) {
+		return true
+	}
+	return false
+}
+
+func (g *Game) canPlayTurn(p Player) bool {
+	if p.Connection == g.CurrentPlayer.Connection && len(g.Players) == 2 {
+		return true
+	}
+	return false
+}
+
+func (g *Game) resetBoard() {
+	for i := range 9 {
+		g.Board[i] = fmt.Sprintf("%d", i)
+	}
+}
+
+func (g *Game) switchCurrentPlayer() {
+	if g.CurrentPlayer.Index == 0 {
+		g.CurrentPlayer = g.Players[1]
+	} else {
+		g.CurrentPlayer = g.Players[0]
+	}
+}
+
+func (g *Game) getScore() string {
+	if len(g.Players) == 2 {
+		return fmt.Sprintf("score: %v:%v - %v:%v", g.Players[0].Symbol, g.Players[0].Score, g.Players[1].Symbol, g.Players[1].Score)
+	}
+	return "waiting for an oponent to join"
+}
+
+func (g *Game) isFreePos(pos int) bool {
+	if g.Board[pos] == colorize("x", "orange") || g.Board[pos] == colorize("o", "cyan") {
+		return false
+	}
+	return true
+}
+
+func (g *Game) playTurn(p Player, pos int) {
+	g.Board[pos] = p.Symbol
+}
+
+func (g *Game) getWinner() string {
+	winCombos := [][]int{
+		{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // verticals
+		{0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // horizontals
+		{0, 4, 8}, {2, 4, 6}, // diagonals
+	}
+
+	for _, combo := range winCombos {
+		if g.Board[combo[0]] == g.Board[combo[1]] && g.Board[combo[1]] == g.Board[combo[2]] {
+			if g.Board[combo[0]] == g.Players[0].Symbol {
+				g.Players[0].Score += 1
+			} else if g.Board[combo[0]] == g.Players[1].Symbol {
+				g.Players[1].Score += 1
+			}
+			return g.Board[combo[0]]
+		}
+	}
+
+	return "none"
+}
+
+func (g *Game) dispatch() {
+	for _, p := range g.Players {
+		p.Connection.Write([]byte(g.printBoard() + "\n" + g.getScore() + "\n"))
+		if p.Connection == g.CurrentPlayer.Connection && len(g.Players) == 2 {
+			p.Connection.Write([]byte("your turn \n"))
+		} else {
+			p.Connection.Write([]byte("oponent's turn \n"))
+		}
+	}
+}
 
 var colors = map[string][2]string{
 	"orange": {"\x1b[34m", "\x1b[0m"},
@@ -29,156 +125,72 @@ func colorize(text string, color string) string {
 	return fmt.Sprintf("%s%s%s", colors[color][0], text, colors[color][1])
 }
 
-func closeConnection(c net.Conn) {
-	for i, p := range players {
-		if p.Connection == c {
-			players = append(players[:i], players[i+1:]...)
-			break
-		}
-	}
-	c.Close()
-}
-
-func handleConnection(conn net.Conn) {
-	scanner := bufio.NewScanner(conn)
+func handleGameConnection(g *Game, p Player) {
+	scanner := bufio.NewScanner(p.Connection)
 	for {
 		ok := scanner.Scan()
 		if !ok {
 			break
 		}
-		play(scanner.Text(), conn)
+		pos, _ := strconv.Atoi(scanner.Text())
+		handlePlayerPosition(pos, g, p)
 	}
-	closeConnection(conn)
+	handlePlayerQuit(g, p)
 
 }
 
-func switchCurrentPlayer() {
-	if currentPlayer.Index == 0 {
-		currentPlayer = players[1]
-	} else {
-		currentPlayer = players[0]
-	}
-}
-
-func getScore() string {
-	if len(players) == 2 {
-		return fmt.Sprintf("score: %v:%v - %v:%v", players[0].Symbol, players[0].Score, players[1].Symbol, players[1].Score)
-	}
-	return "waiting for an oponent to join"
-}
-
-func dispatchBoard() {
-	for _, p := range players {
-		output := printBoard(originalboard)
-		score := getScore()
-		p.Connection.Write([]byte(output + "\n"))
-		p.Connection.Write([]byte(score + "\n"))
-		if p.Connection == currentPlayer.Connection && len(players) == 2 {
-			p.Connection.Write([]byte("your turn \n"))
-		} else {
-			p.Connection.Write([]byte("oponent's turn \n"))
+func handlePlayerQuit(g *Game, p Player) {
+	for i, p := range g.Players {
+		if p.Connection == p.Connection {
+			g.Players = append(g.Players[:i], g.Players[i+1:]...)
+			break
 		}
 	}
+	p.Connection.Close()
 }
 
-func isFreePos(pos int) bool {
-	if originalboard[pos] == colorize("x", "orange") || originalboard[pos] == colorize("o", "cyan") {
-		return false
+func handlePlayerPosition(pos int, g *Game, p Player) {
+	if g.canPlayTurn(p) && g.isFreePos(pos) {
+		g.playTurn(p, pos)
+		g.switchCurrentPlayer()
 	}
-	return true
+
+	if g.shouldResetBoard() {
+		g.resetBoard()
+	}
+
+	g.dispatch()
 }
-
-func getWinner(ps []Player) string {
-	winCombos := [][]int{
-		{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // verticals
-		{0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // horizontals
-		{0, 4, 8}, {2, 4, 6}, // diagonals
-	}
-
-	for _, combo := range winCombos {
-		if originalboard[combo[0]] == originalboard[combo[1]] && originalboard[combo[1]] == originalboard[combo[2]] {
-			if originalboard[combo[0]] == ps[0].Symbol {
-				ps[0].Score += 1
-			}
-			if originalboard[combo[0]] == ps[1].Symbol {
-				ps[1].Score += 1
-			}
-			return originalboard[combo[0]]
-		}
-	}
-
-	return NoWinner
-}
-
-func isFull(b map[int]string) bool {
-	for _, v := range b {
-		if v != colorize("x", "orange") && v != colorize("o", "cyan") {
-			return false
-		}
-	}
-	return true
-}
-
-func shouldReset(w string) bool {
-	if w != NoWinner || (w == NoWinner && isFull(originalboard)) {
-		return true
-	}
-	return false
-}
-
-func play(pos string, c net.Conn) {
-	fmt.Println("> " + pos)
-	position, _ := strconv.Atoi(pos)
-
-	if c == currentPlayer.Connection && len(players) == 2 && isFreePos(position) {
-		originalboard[position] = currentPlayer.Symbol
-		switchCurrentPlayer()
-	}
-	winner := getWinner(players)
-	if shouldReset(winner) {
-		originalboard = initBoard()
-	}
-	dispatchBoard()
-}
-
-func initBoard() map[int]string {
-	board := make(map[int]string)
-	for i := 0; i < 9; i++ {
-		board[i] = fmt.Sprintf("%d", i)
-	}
-	return board
-}
-
-func printBoard(b map[int]string) string {
-	output := ""
-	for i := 0; i < 9; i += 3 {
-		output += fmt.Sprintf("%v | %v | %v\n", b[i], b[i+1], b[i+2])
-	}
-	return output
-}
-
-var originalboard = initBoard()
 
 func main() {
 	listener, _ := net.Listen("tcp", "localhost:8080")
 	fmt.Println("Listening on localhost:8080.")
 
+	var game = Game{
+		Board: make(map[int]string, 9),
+	}
+
+	game.resetBoard()
+
 	defer listener.Close()
 
 	for {
-		conn, _ := listener.Accept() // connect using telnet cmd: telnet localhost 8080
+		conn, _ := listener.Accept()
 		fmt.Printf("client connected from %v\n", conn.RemoteAddr().String())
 
-		if len(players) < 2 {
+		if len(game.Players) < 2 {
 			player := Player{
-				Index:      len(players),
+				Index:      len(game.Players),
 				Connection: conn,
-				Symbol:     []string{colorize("x", "orange"), colorize("o", "cyan")}[len(players)],
+				Symbol:     []string{colorize("x", "orange"), colorize("o", "cyan")}[len(game.Players)],
 				Score:      0,
 			}
-			players = append(players, player)
-			currentPlayer = player
-			go handleConnection(conn)
+			game.Players = append(game.Players, player)
+			game.CurrentPlayer = player
+			go handleGameConnection(&game, player)
+		} else {
+			conn.Write([]byte("Game is full, try again later!" + "\n"))
+			conn.Close() // only accepting 2 player
 		}
 	}
 }
